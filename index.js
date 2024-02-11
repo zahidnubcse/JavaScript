1476d96e97c709575a024b0ff8aa0f2994294105
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-02-11 12:39:44 +0600

    added index
    letter grade

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var marks =prompt("Enter marks:");
 
-if(marks>=80)
+
+if(marks>100 || marks<0 )
+console.log("Invalid");
+else if(marks>=80 && marks<= 100)
  console.log("A+");
-else if(marks>=70)
+else if(marks>=70  && marks<= 79)
  console.log("A");
-else if(marks>=60)
+else if(marks>=60  && marks<= 59)
  console.log("A-");
-else if(marks>=50)
+else if(marks>=50  && marks<=49)
  console.log("B+");
-else if(marks>=40)
+else if(marks>=40  && marks<= 39)
  console.log("B");
-else if(marks>=33)
+else if(marks>=33  && marks==33)
  console.log("B-");
 else
  console.log("Fail");
