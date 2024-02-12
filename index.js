f3ce1903157afaa1ae948fb00e52e43929825d7a
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-02-13 02:14:07 +0600

    added index
    loop & condition of JS

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-var marks =prompt("Enter marks:");
 
-
-if(marks>100 || marks<0 )
-console.log("Invalid");
-else if(marks>=80 && marks<= 100)
- console.log("A+");
-else if(marks>=70  && marks<= 79)
- console.log("A");
-else if(marks>=60  && marks<= 59)
- console.log("A-");
-else if(marks>=50  && marks<=49)
- console.log("B+");
-else if(marks>=40  && marks<= 39)
- console.log("B");
-else if(marks>=33  && marks==33)
- console.log("B-");
-else
- console.log("Fail");
+ var i= 16;
+ do
+ {
+  document.write(" "+i);
+  i++;
+ }
+ while(i<=15)
