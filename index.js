dcbee0c8cdbd6f4f035911758c85dce4f32750fe
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-02-22 22:30:22 +0600

    added index
     ternary operator

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
+var num1= prompt("Enter First Number: ");
+var num2= prompt("Enter Second Number: ");
+var num3= prompt("Enter Third Number: ");
+var result= num1> num2 && num1> num3 ? "Num1 is large": num2>num1 && num2>num3 ? "Num2 is large" : num3>num1 && num3>num2 ? "Num3 is large": "Equal";
 
- var i= 16;
- do
- {
-  document.write(" "+i);
-  i++;
- }
- while(i<=15)
+console.log(result);
