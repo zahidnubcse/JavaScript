fc2fe98c231de09fc665d5f57c1fbd6ff9f302fa
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-03-15 12:10:48 +0600

    index added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var num1= prompt("Enter First Number: ");
-var num2= prompt("Enter Second Number: ");
-var num3= prompt("Enter Third Number: ");
-var result= num1> num2 && num1> num3 ? "Num1 is large": num2>num1 && num2>num3 ? "Num2 is large" : num3>num1 && num3>num2 ? "Num3 is large": "Equal";
-
-console.log(result);
+ var num = new Array();
+ 
+ for(var i=0; i<5; i++)
+ {
+    num[i]= parseInt(prompt("Enter number: "));
+ }
+ var sum= 0;
+ for(var i=0; i<5; i++)
+ {
+    console.log(num[i]);
+   sum= sum+ num[i];
+ }
+ console.log("sum="+sum);
