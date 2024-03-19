6c12c17fdb51f44152c5efab089accc8847b8976
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-03-19 10:27:46 +0600

    added index
    sort method

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
- var num = new Array();
- 
- for(var i=0; i<5; i++)
- {
-    num[i]= parseInt(prompt("Enter number: "));
- }
- var sum= 0;
- for(var i=0; i<5; i++)
- {
-    console.log(num[i]);
-   sum= sum+ num[i];
- }
- console.log("sum="+sum);
+var numbers =[20,10,15,9,5];
+numbers.sort(function(a,b){
+    return a-b;
+})
+document.write(numbers);
