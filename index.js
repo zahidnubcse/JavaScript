a94e39fc9578c71f2213e2f79db2cb1e69110261
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2024-03-19 11:04:26 +0600

    added index
    1D Array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var numbers =[20,10,15,9,5];
-numbers.sort(function(a,b){
-    return a-b;
-})
-document.write(numbers);
+function heightScore(scores)
+{
+ var max = scores[0];
+ for(var x=1; x<scores.length; x++){
+    if(max < scores[x]){
+        max= scores[x];
+    }
+ }
+return max;
+}
+var scores =[20,100,0,56,25];
+var maxScore= heightScore(scores);
+document.write(maxScore);
